92959119aa90f1fa2cf7755b35d9aa8e789377b3
Ghaidaa <38421568+GH8L@users.noreply.github.com>
2022-12-18 01:58:25 +0300

    Update bikershare.py

--- bikershare.py
+++ bikershare.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import numpy as np
 
+
+#refeactoring edited 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
